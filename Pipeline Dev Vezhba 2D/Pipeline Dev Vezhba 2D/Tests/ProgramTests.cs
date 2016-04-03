727696747a24fb5d09ff9b1435fa884c3d2a0c7c
vlado <vlzakar@gmail.com>
2016-04-03 12:52:24 +0200

    Pipeline 2D Vezhba Project added
    Pipeline 2D Vezhba Project added for Python/C++ pipeline developer

---
+++ Pipeline Dev Vezhba 2D/Pipeline Dev Vezhba 2D/Tests/ProgramTests.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: vlado
+ * Date: 30.03.2016
+ * Time: 11:25
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using NUnit.Framework;
+
+namespace Pipeline_Dev_Vezhba_2D.Tests
+{
+	[TestFixture]
+	public class ProgramTests
+	{
+		[Test]
+		public void TestMethod()
+		{
+			// TODO: Add your test.
+		}
+	}
+}
