4ca751b15af653f760faf5d750a95c779d3a9631
bluePlayer <bluePlayer@github.com>
2017-06-12 21:44:04 +0200

    Edited some code about the FMath plugin for TinyMCE editor

--- JQueryPractices/jQueryUI/js/tinymceapp.js
+++ JQueryPractices/jQueryUI/js/tinymceapp.js
@@ -11,12 +11,14 @@ var tinymceapp = (function (jq, tmce) {'use strict';
                 selector: '#editor',
                 height: 500,
                 theme: 'modern',
+
                 plugins: [
                     'advlist autolink lists link image charmap print preview hr anchor pagebreak',
                     'searchreplace wordcount visualblocks visualchars code fullscreen',
                     'insertdatetime media nonbreaking save table contextmenu directionality',
                     'emoticons template paste textcolor colorpicker textpattern imagetools codesample toc FMathEditor'
                 ],
+
                 toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
                 toolbar2: 'print preview media | forecolor backcolor emoticons | codesample | FMathEditor',
 
@@ -34,7 +36,7 @@ var tinymceapp = (function (jq, tmce) {'use strict';
                     img.src = 'img/math.png';
 
                     base64String = canvas.toDataURL('image/png', 1.0);
-                    blob = canvas.toBlob(function (blob) {
+                    /*blob = canvas.toBlob(function (blob) {
                         var newImg = document.createElement('img'),
                         url = window.URL.createObjectURL(blob);
                         console.log(url);
@@ -45,12 +47,14 @@ var tinymceapp = (function (jq, tmce) {'use strict';
 
                         newImg.src = url;
                         //newImg.alt = "MathML (base64):" +
-                        document.body.appendChild(newImg);console.log(blob);
-                    }, 'image/png', 1.0);
+                        document.body.appendChild(newImg);
+                        console.log(blob);
+                    }, 'image/png', 1.0);*/
 
-                    console.log(base64String);//.substring(16, base64String.length));
-                    tmce.activeEditor.insertContent('<img alt="MathML (base64):' + base64String.substring(22, base64String.length) + '" src="' + base64String + '"/>');
+                    //console.log(base64String);//.substring(16, base64String.length));
+                    //tmce.activeEditor.insertContent('<img alt="MathML (base64):' + base64String.substring(22, base64String.length) + '" src="' + base64String + '"/>');
                     //tmce.activeEditor.insertContent('<img alt="Blob Test" src="img/math.png" />');
+
                 }
             });
         }
